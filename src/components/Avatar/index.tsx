4fb3e7b6db9101c48d92d1168d53d7009645497e
Gustavo Ewald <ewaldgustavo@gmail.com>
2024-11-11 00:16:24 -0300

    Estilizacao sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/gustavoEwald.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
-import Title from '../../components/Titulo'
+import Titulo from '../../components/Titulo'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/GustavoEwald.png" alt="" />
-    <Title fontSize={20}>Gustavo Ewald</Title>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Gustavo Ewald</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        GustavoEwald
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
